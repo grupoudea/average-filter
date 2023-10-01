7fbb75cd7a0ab36c1f11346a6bcc9b9d91881df5
berracode <juanguillermoalarcon@gmail.com>
2023-10-01 17:49:17 -0500

    se refactoriza metodos

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 def generate_kernel(n):
     kernel = np.ones((n, n), np.float32) / (n * n)
-    print(f'Kenel:\n{kernel}\nSize: {n}')
+    # print(f'Kenel:\n{kernel}\nSize: {n}')
     return kernel
 
 
@@ -19,6 +19,12 @@ def draw_plot(img, title):
     plt.yticks([])
 
 
+def read_rgb_img(imgpath):
+    img = cv.imread(imgpath)  # la imagen se lee como BGR
+    img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # con esto la paso a RGB
+    return img_rgb
+
+
 def main():
     parser = argparse.ArgumentParser(description='Average filter')
 
@@ -27,26 +33,27 @@ def main():
     parser.add_argument('impath', type=str, help='image path')
 
     args = parser.parse_args()
-    print("args", args)
+    kernel = generate_kernel(args.a)
+    print(f'=================\n'
+          f'Filter: {args.filter}\n'
+          f'Kernel:\n'
+          f'({args.a}x{args.a})\n'
+          f'=================')
+
 
     if args.filter == 'avglib':
-        kernel = generate_kernel(args.a)
-        img = cv.imread(args.impath)  # la imagen se lee como BGR
-        img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # con esto la paso a RGB
-        draw_plot(img_rgb, 'original')
-        result = cv.filter2D(img_rgb, -1, kernel)
-        draw_plot(result, 'result')
-        plt.grid(True)
-
-        plt.show()
-
+        img = read_rgb_img(args.impath)
+        result = cv.filter2D(img, -1, kernel)
 
     elif args.filter == 'avgown':
-        kernel = generate_kernel(args.a)
+        print("Implement code for own average filter function")
     elif args.filter == 'avgboth':
-        kernel = generate_kernel(args.a)
+        #  TODO
+        print("TODO")
 
-    print(f'Resultado de {args.filter}: {kernel}')
+    draw_plot(img, 'original')
+    draw_plot(result, 'result')
+    plt.show()
 
 
 if __name__ == '__main__':
